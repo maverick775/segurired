3135a412c2f754fd25c1cbfaa03b57c1e35dd8ef
PakePerez <pako.perez@outlook.com>
2020-07-25 00:18:43 -0500

    async await added to fetch call

--- sls/pilot-test/lambdas/getDeviceAtt.js
+++ sls/pilot-test/lambdas/getDeviceAtt.js
@@ -1,8 +1,8 @@
 'use strict'
 const fetch = require('node-fetch');
 
-module.exports.getAtts = async event => {
-  fetch('http://ec2-3-101-90-91.us-west-1.compute.amazonaws.com:8080/api/v1/A1_TEST_TOKEN/attributes?sharedKeys=active')
+module.exports.getAtts = async (event,context) => {
+  await fetch('http://ec2-3-101-90-91.us-west-1.compute.amazonaws.com:8080/api/v1/A1_TEST_TOKEN/attributes?sharedKeys=active')
     .then(function (response) {
       return response.json();
     })
