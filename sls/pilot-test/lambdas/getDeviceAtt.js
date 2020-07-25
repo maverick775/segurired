c36208e6624ae2de2f067081b822b3be856b775b
PakePerez <pako.perez@outlook.com>
2020-07-25 00:06:20 -0500

    node-fetch added to modules

--- sls/pilot-test/lambdas/getDeviceAtt.js
+++ sls/pilot-test/lambdas/getDeviceAtt.js
@@ -1,4 +1,5 @@
 'use strict'
+const fetch = require('node-fetch');
 
 module.exports.getAtts = async event => {
   fetch('http://ec2-3-101-90-91.us-west-1.compute.amazonaws.com:8080/api/v1/A1_TEST_TOKEN/attributes?sharedKeys=active')
