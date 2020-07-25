0dbb856fe0fbc5871c4f0ef689ba25f36e6d936d
PakePerez <pako.perez@outlook.com>
2020-07-25 15:06:55 -0500

    Conosle log agregado para prueba

--- sls/pilot-test/lambdas/getDeviceAtt.js
+++ sls/pilot-test/lambdas/getDeviceAtt.js
@@ -2,12 +2,14 @@
 const fetch = require('node-fetch');
 
 module.exports.getAtts = async (event,context) => {
-  await fetch('http://ec2-3-101-90-91.us-west-1.compute.amazonaws.com:8080/api/v1/A1_TEST_TOKEN/attributes?sharedKeys=active')
+    
+  await fetch('http://ec2-3-101-90-91.us-west-1.compute.amazonaws.com:8080/api/v1/A2_TEST_TOKEN/attributes?sharedKeys=state')
     .then(function (response) {
       return response.json();
     })
     .then(function (myJson) {
+      console.log('*************************************************** ANSWER ****************************************');  
       console.log(myJson);
     });
+    
 };
-
